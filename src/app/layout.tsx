2c5c3705e8d6ff0b048324e70c63ded219f89b42
K-njeru <knjeru519@gmail.com>
2025-03-15 23:56:05 +0300

    `Update globals.css and layout.tsx to use Urbanist font`

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,13 @@
 import type { Metadata } from "next";
+import { Urbanist } from "next/font/google";
 import "./globals.css";
+
+// Load the Manrope font
+const urbanist = Urbanist({
+  subsets: ['latin'], // Specify the subset(s) you need
+  weight: ['400', '500', '600', '700'], // Specify the weights you need
+  variable: '--font-urbanist', // Define a CSS variable for the font
+});
 
 // Metadata remains the same
 export const metadata: Metadata = {
@@ -13,7 +21,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning className={`${urbanist.variable} font-sans`}>
       <body className="font-tahoma antialiased">
         <main>
           {children}
